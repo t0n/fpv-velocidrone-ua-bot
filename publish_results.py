00ff6c3b4f35efaeba1401b4f0ae559e80b300fd
t0n <anton.koba@gmail.com>
2020-11-11 13:12:18 +0200

    track history

--- publish_results.py
+++ publish_results.py
@@ -11,6 +11,7 @@ logging.basicConfig(filename='log.txt', filemode='a',
                     format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
                     # datefmt='%H:%M:%S',
                     level=logging.DEBUG)
+logging.getLogger('telegram').setLevel(logging.ERROR)
 
 
 def main():
--- select_track.py
+++ select_track.py
@@ -13,6 +13,7 @@ logging.basicConfig(filename='log.txt', filemode='a',
                     format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
                     # datefmt='%H:%M:%S',
                     level=logging.DEBUG)
+logging.getLogger('telegram').setLevel(logging.ERROR)
 
 
 def main():
